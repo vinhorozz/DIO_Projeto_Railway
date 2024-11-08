81dec11167ab26985984cc329db6c5aa88093197
Vinho <vinhorozz@hotmail.com>
2024-11-08 11:12:49 -0300

    Correção do scale e precision em card e Account

--- src/main/java/Edu/flavio/projeto/railway/demo/DemoApplication.java
+++ src/main/java/Edu/flavio/projeto/railway/demo/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("http://localhost:9090/");
+		System.out.println("http://localhost:9090/user");
+		System.out.println("http://localhost:9090/h2-console/");
 	}
 
 }
